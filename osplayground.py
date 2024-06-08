a9385b0d802780f6edc4d2918dd0108b27e46e3b
Michael Hensley <29671149+ZRexshima@users.noreply.github.com>
2024-06-08 22:22:27 +0900

    removes shadowing

--- osplayground.py
+++ osplayground.py
@@ -2,35 +2,35 @@ import os
 from pathlib import Path
 
 
-def exists(filename: str) -> bool:
-    p = Path(filename)
+def exists(filename: Path) -> bool:
+    p: Path = Path(filename)
     return p.exists()
 
 
-def file_exists(filename: str) -> bool:
+def file_exists(filename: Path) -> bool:
     f = Path(filename)
     return f.is_file()
 
 
-def dir_exists(dirname: str) -> bool:
-    d = Path(dirname)
+def dir_exists(directory: Path) -> bool:
+    d = Path(directory)
     return d.is_dir()
 
 
 if __name__ == '__main__':
-    p = Path('fake-file')
-    d = Path('fake-directory')
+    my_file = Path('fake-file')
+    my_dir = Path('fake-directory')
 
     os.system('touch fake-file')
 
-    assert exists(p) is True
-    assert file_exists(p) is True
+    assert exists(my_file) is True
+    assert file_exists(my_file) is True
 
     os.system('rm fake-file')
 
     os.system('mkdir fake-directory')
 
-    assert exists(d) is True
-    assert dir_exists(d) is True
+    assert exists(my_dir) is True
+    assert dir_exists(my_dir) is True
 
     os.system('rmdir fake-directory')
